1661c6c8360fc2106e221136be3af51cd678c47f
beatriz <a044416@umaia.pt>
2024-12-02 15:50:21 +0000

    update position

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,6 +1,7 @@
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.graphics.TextGraphics;
 import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
@@ -16,31 +17,24 @@ public class Game {
     private int width;
     private int height;
     private Snake s;
+    private int grid;
 
     public Game() throws IOException {
+        s = new Snake(grid, grid);
 
-        s = new Snake();
-
+        this.grid = 1;
         this.width = 30;
         this.height = 30;
-
         TerminalSize terminalSize = new TerminalSize(width, height);
-
         DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory()
                 .setInitialTerminalSize(terminalSize)
                 .setForceAWTOverSwing(true);
-
         Terminal terminal = terminalFactory.createTerminal();
-
         screen = new TerminalScreen(terminal);
         screen.setCursorPosition(null);
         screen.startScreen();
         screen.doResizeIfNecessary();
-
-
     }
-
-
     public void draw() throws IOException {
         screen.clear();
         TextGraphics graphics = screen.newTextGraphics();
@@ -50,16 +44,37 @@ public class Game {
         s.update();
         screen.refresh();
     }
-
     public void run() throws IOException {
         while (true) {
             draw();
+            KeyStroke key = screen.pollInput();
+
+
+            if (key != null) {
+                processKey(key);
+            }
 
             try {
                 Thread.sleep(100);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+        }
+    }
+
+    private void processKey(KeyStroke key) throws IOException {
+        if (key.getKeyType() == KeyType.Character && key.getCharacter() == 'w') {
+            s.dir(0, -1);
+        } else if (key.getKeyType() == KeyType.Character && key.getCharacter() == 'd') {
+            s.dir(1, 0);
+        } else if (key.getKeyType() == KeyType.Character && key.getCharacter() == 'a') {
+            s.dir(-1, 0);
+        } else if (key.getKeyType() == KeyType.Character && key.getCharacter() == 's') {
+            s.dir(0, 1);
+        } else if (key.getKeyType() == KeyType.Character && key.getCharacter() == 'q') {
+            screen.close();
+        } else if (key.getKeyType() == KeyType.EOF) {
+            System.exit(0);
         }
     }
 }
--- src/main/java/Snake.java
+++ src/main/java/Snake.java
@@ -7,21 +7,40 @@ import com.googlecode.lanterna.graphics.TextGraphics;
 public class Snake {
     public int x, y; // Coordenadas da cobra
     public int xspeed, yspeed;
+    public int grid;
 
-    public Snake(){
-        this.x = 1;
-        this.y = 1;
+    public Snake(int x, int y){
+        this.x = x;
+        this.y = y;
         this.xspeed = 1; // Movimento inicial para a direita
         this.yspeed = 0;
+        this.grid = 1;
     }
 
     public void update(){
         this.x += xspeed; // Atualiza a posição baseada na velocidade
         this.y += yspeed;
+
+
+        if (this.x > 29){
+            this.x = 29;
+        } else if (this.x < 0){
+            this.x = 0;
+        } else if (this.y > 29){
+            this.y = 29;
+        } else if (this.y < 0){
+            this.y = 0;
+        }
     }
 
     public void show(TextGraphics graphics){
         graphics.setBackgroundColor(TextColor.Factory.fromString("#FFFFFF"));
         graphics.fillRectangle(new TerminalPosition(this.x, this.y), new TerminalSize(1, 1), ' '); // Desenha a cobra
+    }
+    public void dir(int x, int y){
+        this.xspeed = x;
+        this.yspeed= y;
+        this.xspeed = x*grid;
+        this.yspeed= y*grid;
     }
 }
